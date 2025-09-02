d7e0ae9241138ea455f002e993d74da8b134e091
Isma847 <castilloismael8471@gmail.com>
2025-09-01 21:26:40 -0300

    Delete mapa.js

--- mapa.js
+++
@@ -1,6 +0,0 @@
-var map = L.map('mapa').setView([-37.997794, -57.548122], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
